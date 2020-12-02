a71e15f21622cdb292d09455765653b58e2cafef
Jonas Otto (ottojo) <jottosmail@gmail.com>
2020-12-02 13:08:39 +0100

    day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,26 +1,38 @@
-use std::convert::TryInto;
 use std::env;
 use std::fs;
 
 #[derive(Debug)]
 struct Policy {
-    min: u64,
-    max: u64,
+    min: usize,
+    max: usize,
     character: char,
 }
 
 fn is_valid(pol: Policy, pw: &str) -> bool {
     let count = pw.matches(pol.character).count();
-    if count < pol.min.try_into().unwrap() || count > pol.max.try_into().unwrap() {
+    if count < pol.min || count > pol.max {
         return false;
     }
     return true;
+}
+
+fn is_valid_type2(pol: Policy, pw: &str) -> bool {
+    if (pw.chars().nth(pol.min - 1).unwrap() == pol.character
+        && pw.chars().nth(pol.max - 1).unwrap() != pol.character)
+        || (pw.chars().nth(pol.min - 1).unwrap() != pol.character
+            && pw.chars().nth(pol.max - 1).unwrap() == pol.character)
+    {
+        return true;
+    }
+
+    return false;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
+    let policyType = &args[2];
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let lines = contents.split("\n").collect::<Vec<&str>>();
@@ -34,8 +46,8 @@ fn main() {
         let dash = line.find("-").expect("Line contains no dash");
         let colon = line.find(":").expect("Line contains no colon");
         println!("Dash at index {:?}, colon at {:?}", dash, colon);
-        let min: u64 = line[0..dash].parse().expect("Minimum not found");
-        let max: u64 = line[dash + 1..colon - 2]
+        let min: usize = line[0..dash].parse().expect("Minimum not found");
+        let max: usize = line[dash + 1..colon - 2]
             .parse()
             .expect("Maximum not found");
         println!("Min: {:?}, Max: {:?}", min, max);
@@ -49,8 +61,18 @@ fn main() {
         println!("Policy: {:?}", policy);
         let pw = &line[colon + 2..];
         println!("PW: {:?}", pw);
-        if is_valid(policy, pw) {
-            count += 1;
+        if policyType == "1" {
+            if is_valid(policy, pw) {
+                println!("Valid (type 1)");
+                count += 1;
+            }
+        } else if policyType == "2" {
+            if is_valid_type2(policy, pw) {
+                println!("Valid (type 2)");
+                count += 1;
+            }
+        } else {
+            panic!("Invalid policy type");
         }
     }
 
